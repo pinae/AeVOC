63af7c49afb57ce10a3822f38c81323a609d0ca6
pinae <pina@pinae.net>
2020-12-08 13:22:59 +0100

    using multi line string

--- src/webHandlers.cpp
+++ src/webHandlers.cpp
@@ -12,10 +12,11 @@ void handleRoot() {
     // -- Captive portal request were already served.
     return;
   }
-  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
-  s += "<title>IotWebConf 01 Minimal</title></head><body>Hello world!";
-  s += "Go to <a href='config'>configure page</a> to change settings.";
-  s += "</body></html>\n";
+  String s = "<!DOCTYPE html><html lang=\"en\"><head>"
+  "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>"
+  "<title>IotWebConf 01 Minimal</title></head><body>Hello world!"
+  "Go to <a href='config'>configure page</a> to change settings."
+  "</body></html>\n";
 
   server.send(200, "text/html", s);
 }
